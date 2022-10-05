d1224e0b13bab5e35c425e7333769ee713561754
david.vivo <David.Vivo@beezy.net>
2022-10-05 16:51:54 +0200

    feat: add modified property

--- src/components/molecules/CharactersList/CharactersList.tsx
+++ src/components/molecules/CharactersList/CharactersList.tsx
@@ -1,27 +1,52 @@
 export interface Thumbnail {
-  path: string
-  extension: string
+  path: string;
+  extension: string;
 }
 
 export interface Character {
-  id: number
-  name: string
-  description: string
-  thumbnail: Thumbnail
+  description: string;
+  id: number;
+  name: string;
+  modified: string;
+  thumbnail: Thumbnail;
 }
 
 export interface CharactersListProps {
-  characters: Character[]
+  characters: Character[];
 }
 
 const CharactersList = ({ characters }: CharactersListProps) => {
   return (
     <ul>
       {characters.map((character) => (
-        <li key={character.id}>{character.name}</li>
+        <li key={character.id}>
+          <h2>{character.name}</h2>
+          <p>
+            <small>
+              <strong>thumbnail: </strong>
+              {character.thumbnail.path}
+            </small>
+          </p>
+          <p>
+            <small>
+              <strong>modified: </strong>
+              {character.modified}
+            </small>
+          </p>
+          {character.description ? (
+            <p>
+              <strong>DESCRIPTION: </strong>
+              {character.description}
+            </p>
+          ) : (
+            <p>
+              <strong>DESCRIPTION NOT AVAILABLE</strong>
+            </p>
+          )}
+        </li>
       ))}
     </ul>
-  )
-}
+  );
+};
 
 export default CharactersList;
