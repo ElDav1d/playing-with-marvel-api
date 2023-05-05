2ab9cec5b923104825a0ab1c37108e1ea524e5a6
ElDav1d <dvivoes@yahoo.es>
2023-05-05 11:40:26 +0200

    display comic data

--- src/components/molecules/ComicsListItem/ComicsListItem.tsx
+++ src/components/molecules/ComicsListItem/ComicsListItem.tsx
@@ -1,13 +1,46 @@
 import { CharacterComicDetails } from '@/components/pages/CharacterDetail/interfaces/characterComics';
-
+import Image from '@/components/atoms/Image/Image';
+import { useMemo } from 'react';
 export interface ComicsListItemProps {
   comic: CharacterComicDetails;
 }
 
 export const ComicsListItem = ({ comic }: ComicsListItemProps) => {
+  const dateTypes = ['onsaleDate', 'focDate'];
+
+  const filteredDates = useMemo(() => {
+    return comic?.dates?.filter((date) => dateTypes.includes(date.type));
+  }, [comic]);
+
   return (
     <li>
+      <Image
+        path={comic.images[0].path}
+        extension={comic.images[0].extension}
+        variant='standard_small'
+      />
       <h3>{comic.title}</h3>
+      <p>{comic.description}</p>
+      <p>
+        <small>
+          <strong>Issue: </strong>
+          {comic.issueNumber}
+        </small>
+      </p>
+      <p>
+        <small>
+          <strong>Modified: </strong>
+          {comic.modified}
+        </small>
+      </p>
+      {filteredDates.map((date) => (
+        <p key={date.date}>
+          <small>
+            <strong>{date.type}: </strong>
+            {date.date}
+          </small>
+        </p>
+      ))}
     </li>
   );
 };
