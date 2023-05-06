b2324210789d00140a45345dfd15d7d605812087
ElDav1d <dvivoes@yahoo.es>
2023-05-06 13:27:26 +0200

    test characters standards

--- src/components/pages/Characters/__tests__/Characters.test.tsx
+++ src/components/pages/Characters/__tests__/Characters.test.tsx
@@ -8,6 +8,8 @@ import Characters from '../Characters';
 
 jest.mock('../hooks');
 
+const mockUseCharacters = useCharacters as jest.Mock;
+
 const mockIntersectionObserver = jest.fn();
 
 mockIntersectionObserver.mockReturnValue({
@@ -18,14 +20,12 @@ mockIntersectionObserver.mockReturnValue({
 
 window.IntersectionObserver = mockIntersectionObserver;
 
+const queryClient = new QueryClient();
+
 describe(Characters, () => {
   it('renders a page of characters', () => {
     // ARRANGE
     const charactersAZ = JSON.parse(JSON.stringify(mockCharactersAZ));
-
-    const queryClient = new QueryClient();
-
-    const mockUseCharacters = useCharacters as jest.Mock;
 
     mockUseCharacters.mockReturnValue({
       isLoading: false,
@@ -47,6 +47,60 @@ describe(Characters, () => {
       screen.getByRole('heading', { name: /this is the characters page/i }),
     ).toBeInTheDocument();
     expect(screen.getByRole('heading', { name: /animal/i })).toBeInTheDocument();
-    expect(screen.getByRole('heading', { name: /Braineater/i })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: /braineater/i })).toBeInTheDocument();
+  });
+
+  it('renders a list with characters', () => {
+    // ARRANGE
+    const charactersAZ = JSON.parse(JSON.stringify(mockCharactersAZ));
+
+    mockUseCharacters.mockReturnValue({
+      isLoading: false,
+      isError: false,
+      characters: charactersAZ,
+    });
+
+    // ACT
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <Characters />
+        </Router>
+      </QueryClientProvider>,
+    );
+
+    const lists = screen.getAllByRole('list');
+    const listTarget = lists.find((list) => list.textContent?.match(/animal/i));
+
+    // ASSERT
+    expect(listTarget).toBeInTheDocument();
+  });
+
+  it('renders a list with characters as list items', () => {
+    // ARRANGE
+    const charactersAZ = JSON.parse(JSON.stringify(mockCharactersAZ));
+
+    mockUseCharacters.mockReturnValue({
+      isLoading: false,
+      isError: false,
+      characters: charactersAZ,
+    });
+
+    // ACT
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <Characters />
+        </Router>
+      </QueryClientProvider>,
+    );
+
+    const listItems = screen.getAllByRole('listitem');
+    const listItemTargetOne = listItems.find((item) => item.textContent?.match(/animal/i));
+    const listItemTargetTwo = listItems.find((item) => item.textContent?.match(/braineater/i));
+
+    // ASSERT
+    expect(listItemTargetOne).toBeInTheDocument();
+    expect(listItemTargetTwo).toBeInTheDocument();
   });
 });
