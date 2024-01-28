01816876abc941e48010ed33295d84eeef688628
ElDav1d <dvivoes@yahoo.es>
2024-01-28 18:30:46 +0100

    apply tablet and desktop grid

--- src/components/organisms/CharactersList/CharactersList.tsx
+++ src/components/organisms/CharactersList/CharactersList.tsx
@@ -7,7 +7,7 @@ export interface CharactersListProps {
 const CharactersList = ({ characters }: CharactersListProps) => {
   console.log(characters);
   return (
-    <ul className='grid gap-3 grid-cols-2 sm:grid-cols-4'>
+    <ul className='grid gap-3 grid-cols-2 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6'>
       {characters.map(({ id, name, thumbnail, description }) => (
         <CharacterListItem
           key={id}
