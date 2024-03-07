509c90d4987b94f0935eda0063b93d3cd16730e1
ElDav1d <dvivoes@yahoo.es>
2024-03-07 11:07:06 +0100

    extract characters hero section

--- src/components/pages/Characters/Characters.tsx
+++ src/components/pages/Characters/Characters.tsx
@@ -23,6 +23,7 @@ import { RingLoader } from 'react-spinners';
 import SideDrawer from '@/components/organisms/SideDrawer';
 import CharactersControlPanel from '@/components/organisms/CharactersControlPanel';
 import { ControlPanelInfo } from '@/components/molecules/ControlPanelInfo';
+import { CharactersHeroSection } from '@/components/organisms/CharactersHeroSection';
 
 const Characters = () => {
   const [searchInput, setSearchInput] = useState<string>('');
@@ -144,47 +145,33 @@ const Characters = () => {
         </SideDrawer>
       </Header>
       <Container element={'main'}>
-        <section
-          className='relative z-0 flex flex-col items-center justify-around gap-2 min-h-52 md:min-h-72 mb-4 bg-black bg-hero-image bg-center bg-cover
-        before:absolute before:content[""] before:h-full before:w-full  before:bg-gradient-to-b before:from-trans-0.75-black before:from-50% before:left-0 before:top-0 before:z-[-1]'
-        >
-          <Container element={'div'} className='flex flex-col gap-4'>
-            <div className='w-full text-white text-center'>
-              <h2 className='text-2xl md:text-3xl font-semibold text-white text-center uppercase mb-1'>
-                Marvel Characters
-              </h2>
-              <p className='text-sm md:text-base text-center'>
-                Get hooked on a hearty helping of heroes and villains from the humble House of
-                Ideas!
-              </p>
-            </div>
+        <CharactersHeroSection>
+          <CharactersControlPanel
+            isDesktop
+            searchInput={searchInput}
+            setSearchInput={setSearchInput}
+            searchTitle={'Search by name'}
+            searchPlaceholder={'type a character name'}
+            setOnClearData={setOnClearData}
+            isEmptyData={!isFetching && filteredCharacters?.length === 0}
+            emptyDataLiteral={EMPTY_DATA_LITERAL_LIST}
+            orderTitle='Order results'
+            onOrderChange={(event) => orderHandler(event)}
+            orderOptions={Object.values(FetchingOrder)}
+            orderLiterals={Object.values(HumanizedOrder)}
+            filtersTitle='Filter results:'
+            filtersOptions={Object.values(FilterCriteria)}
+            filtersLiterals={Object.values(FilterCriteria)}
+            setFilters={setFilters}
+            setOnClearChecks={handleClearChecks}
+            onClearChecks={onClearFilters}
+          />
 
-            <CharactersControlPanel
-              isDesktop
-              searchInput={searchInput}
-              setSearchInput={setSearchInput}
-              searchTitle={'Search by name'}
-              searchPlaceholder={'type a character name'}
-              setOnClearData={setOnClearData}
-              isEmptyData={!isFetching && filteredCharacters?.length === 0}
-              emptyDataLiteral={EMPTY_DATA_LITERAL_LIST}
-              orderTitle='Order results'
-              onOrderChange={(event) => orderHandler(event)}
-              orderOptions={Object.values(FetchingOrder)}
-              orderLiterals={Object.values(HumanizedOrder)}
-              filtersTitle='Filter results:'
-              filtersOptions={Object.values(FilterCriteria)}
-              filtersLiterals={Object.values(FilterCriteria)}
-              setFilters={setFilters}
-              setOnClearChecks={handleClearChecks}
-              onClearChecks={onClearFilters}
-            />
+          {listControlInfoItems && listControlInfoItems.length > 0 && (
+            <ControlPanelInfo infoItems={listControlInfoItems} onClear={handleClear} />
+          )}
+        </CharactersHeroSection>
 
-            {listControlInfoItems && listControlInfoItems.length > 0 && (
-              <ControlPanelInfo infoItems={listControlInfoItems} onClear={handleClear} />
-            )}
-          </Container>
-        </section>
         {isError && <h2>Oooops, there&apos;s an unexpected error...try reloading again!</h2>}
 
         {isFetching && !isFetchingNextPage && (
