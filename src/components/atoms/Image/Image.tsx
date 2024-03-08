5a6f794264390bcb03d1bc69548f16ff529088c9
ElDav1d <dvivoes@yahoo.es>
2024-03-08 14:38:10 +0100

    FIX image multiple sizing

--- src/components/atoms/Image/Image.tsx
+++ src/components/atoms/Image/Image.tsx
@@ -9,12 +9,33 @@ import './Image.css';
  * @typedef
  * Type for picture size variants
  */
-export type PicVariant =
+export type PicVariantName =
   | 'standard_small'
   | 'standard_xlarge'
   | 'standard_fantastic'
   | 'landscape_amazing'
   | 'landscape_incredible';
+
+/**
+ * @typedef
+ * Type for picture width size values
+ * in pixels for each variant
+ *  */
+export type PicVariantWidthValue = 65 | 200 | 250 | 464;
+
+export type PicVariantWidths = {
+  [key in PicVariantName]: PicVariantWidthValue;
+};
+
+/* eslint-disable camelcase */
+const PIC_VARIANT_WIDTHS: PicVariantWidths = {
+  standard_small: 65,
+  standard_xlarge: 200,
+  standard_fantastic: 250,
+  landscape_amazing: 250,
+  landscape_incredible: 464,
+};
+/* eslint-enable camelcase */
 
 /**
  * @interface ImageProps
@@ -30,7 +51,7 @@ export interface ImageProps extends Thumbnail {
    * Array of image URL
    * for art direction
    */
-  sizing: PicVariant | PicVariant[];
+  sizing: PicVariantName | PicVariantName[];
   /**
    * @property {string}
    * Additional class name for <picture>.
@@ -75,32 +96,72 @@ const Image = ({
   const LAZYLOAD_SIZING_DEFAULT = 'standard_small';
   const LAZYLOAD_THRESHOLD = 50;
 
-  const getSrc = (sizing: PicVariant | PicVariant[]) => {
+  const orderSizes = (sizes: PicVariantName[]) => {
+    return sizes.sort((a, b) => {
+      return PIC_VARIANT_WIDTHS[a] - PIC_VARIANT_WIDTHS[b];
+    });
+  };
+
+  const getSrc = (sizing: PicVariantName | PicVariantName[]) => {
     if (typeof sizing === 'string') {
       return `${path}/${sizing}.${extension}`;
     }
-    return `${path}/${sizing[0]}.${extension}`;
+
+    if (Array.isArray(sizing) && sizing.length === 1) {
+      return `${path}/${sizing[0]}.${extension}`;
+    }
+
+    const getBiggestPic = (picSizes: PicVariantName[]) => {
+      return `${path}/${orderSizes(picSizes)[picSizes.length - 1]}.${extension}`;
+    };
+
+    return getBiggestPic(sizing);
   };
 
   const getSrcSet = () => {
-    if (Array.isArray(sizing) && sizing.length > 1) {
-      const uniqueValues = Array.from(new Set(sizing));
+    const hasMultipleSizes = Array.isArray(sizing) && sizing.length > 1;
 
-      if (uniqueValues.length > 1) {
-        return `${path}/${uniqueValues[1]}.${extension}`;
+    if (hasMultipleSizes) {
+      const uniquePicSizes = Array.from(new Set(sizing));
+
+      if (uniquePicSizes.length > 1) {
+        const getPathAndSize = (size: PicVariantName) =>
+          `${path}/${size}.${extension} ${PIC_VARIANT_WIDTHS[size]}w`;
+
+        return orderSizes(uniquePicSizes)
+          .map((size) => getPathAndSize(size))
+          .join(', ');
       }
     }
-    return '';
+
+    return undefined;
   };
 
-  const getBreakpointSizes = () => {
-    let sizes = '';
+  const getBreakpointSizes = (sizing: PicVariantName | PicVariantName[]) => {
+    const hasSingleSize =
+      typeof sizing === 'string' ||
+      (Array.isArray(sizing) && (sizing.length <= 1 || new Set(sizing).size === 1));
 
-    for (const step in MEDIA_BREAKPOINTS) {
-      sizes = `${sizes} (min-width: ${MEDIA_BREAKPOINTS[step as BreakpointStepName]}px)`;
+    if (hasSingleSize) {
+      return undefined;
     }
 
-    return sizes;
+    const uniqueSizes = Array.from(new Set(sizing));
+    const breakpointKeys = Object.keys(MEDIA_BREAKPOINTS);
+
+    const sizes = uniqueSizes.map((size, i) => {
+      const width = `${PIC_VARIANT_WIDTHS[size]}px`;
+      const isNotLastSize = i < breakpointKeys.length && i < uniqueSizes.length - 1;
+
+      if (isNotLastSize) {
+        const step = breakpointKeys[i] as BreakpointStepName;
+        return `(max-width: ${MEDIA_BREAKPOINTS[step]}px) ${width}`;
+      } else {
+        return width;
+      }
+    });
+
+    return sizes.join(', ');
   };
 
   const getAltText = () => {
@@ -116,7 +177,7 @@ const Image = ({
         wrapperClassName='w-full'
         className={`w-full ${classNameContent}`}
         srcSet={getSrcSet()}
-        sizes={getBreakpointSizes()}
+        sizes={getBreakpointSizes(sizing)}
         src={getSrc(sizing)}
         title={title}
         alt={getAltText()}
--- src/components/atoms/Image/__tests__/Image.test.tsx
+++ src/components/atoms/Image/__tests__/Image.test.tsx
@@ -1,56 +1,147 @@
 import { render, screen } from '@testing-library/react';
-import Image, { PicVariant } from '../Image';
+import Image, { PicVariantName } from '../Image';
 
 jest.mock('react-lazy-load-image-component', () => ({
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
-  LazyLoadImage: ({ placeholderSrc, ...props }: any) => <img alt='' {...props} />,
+  LazyLoadImage: ({ placeholderSrc, wrapperClassName, ...props }: any) => <img alt='' {...props} />,
 }));
 
-describe('ImageComponent', () => {
-  it('renders being accessible', () => {
-    // ARRANGE
-    const TITLE = 'Spiderman';
-    const ALT = 'The pic of Spiderman';
-    const PATH = 'http://i.annihil.us/u/prod/marvel/i/mg/3/50/526548a343e4b';
-    const EXTENSION = 'jpg';
-    const VARIANT = 'standard_xlarge';
+it('renders being accessible', () => {
+  // ARRANGE
+  const TITLE = 'Spiderman';
+  const ALT = 'The pic of Spiderman';
+  const PATH = 'http://i.annihil.us/u/prod/marvel/i/mg/3/50/526548a343e4b';
+  const EXTENSION = 'jpg';
+  const VARIANT: PicVariantName = 'standard_xlarge';
 
-    // ACT
-    render(<Image title={TITLE} alt={ALT} path={PATH} extension={EXTENSION} sizing={VARIANT} />);
+  // ACT
+  render(<Image title={TITLE} alt={ALT} path={PATH} extension={EXTENSION} sizing={VARIANT} />);
 
-    // ASSERT
-    expect(screen.getByRole('img', { name: /the pic of spiderman/i })).toBeInTheDocument();
-  });
+  // ASSERT
+  expect(screen.getByRole('img', { name: /the pic of spiderman/i })).toBeInTheDocument();
+});
 
-  it('provides appropiate pic info to screenreaders', () => {
-    // ARRANGE
-    const TITLE = 'Aaron Stack';
-    const ALT = 'The pic of Aaron Stack';
-    const PATH = 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available';
-    const EXTENSION = 'jpg';
-    const VARIANT = 'standard_xlarge';
+it('provides appropiate pic info to screenreaders', () => {
+  // ARRANGE
+  const TITLE = 'Aaron Stack';
+  const ALT = 'The pic of Aaron Stack';
+  const PATH = 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available';
+  const EXTENSION = 'jpg';
+  const VARIANT: PicVariantName = 'standard_xlarge';
 
-    // ACT
-    render(<Image title={TITLE} alt={ALT} path={PATH} extension={EXTENSION} sizing={VARIANT} />);
+  // ACT
+  render(<Image title={TITLE} alt={ALT} path={PATH} extension={EXTENSION} sizing={VARIANT} />);
 
-    // ASSERT
-    expect(screen.getByRole('img', { name: /is not available/i })).toBeInTheDocument();
-  });
+  // ASSERT
+  expect(screen.getByRole('img', { name: /is not available/i })).toBeInTheDocument();
+});
 
-  it('only does art direction with unique elements', () => {
-    // ARRANGE
-    const TITLE = 'Spiderman';
-    const ALT = 'The pic of Spiderman';
-    const PATH = 'http://i.annihil.us/u/prod/marvel/i/mg/3/50/526548a343e4b';
-    const EXTENSION = 'jpg';
-    const VARIANTS: PicVariant[] = ['standard_fantastic', 'standard_fantastic'];
+it('only does art direction with unique elements', () => {
+  // ARRANGE
+  const TITLE = 'Spiderman';
+  const ALT = 'The pic of Spiderman';
+  const PATH = 'http://i.annihil.us/u/prod/marvel/i/mg/3/50/526548a343e4b';
+  const EXTENSION = 'jpg';
+  const VARIANTS: PicVariantName[] = ['standard_fantastic', 'standard_fantastic'];
 
-    // ACT
-    render(<Image title={TITLE} alt={ALT} path={PATH} extension={EXTENSION} sizing={VARIANTS} />);
-    const img = screen.getByRole('img', { name: /the pic of spiderman/i });
+  // ACT
+  render(<Image title={TITLE} alt={ALT} path={PATH} extension={EXTENSION} sizing={VARIANTS} />);
+  const img = screen.getByRole('img', { name: /the pic of spiderman/i });
 
-    // ASSERT
-    expect(img.getAttribute('src')).not.toEqual(img.getAttribute('srcset'));
-    expect(img.getAttribute('srcset')).toEqual('');
-  });
+  // ASSERT
+  expect(img.getAttribute('src')).not.toEqual(img.getAttribute('srcset'));
+  expect(img.getAttribute('srcset')).toEqual(null);
+});
+
+it('renders srcset with size ordered multiple unique sizes', () => {
+  const TITLE = 'Spiderman';
+  const ALT = 'The pic of Spiderman';
+  const PATH = 'http://i.annihil.us/u/prod/marvel/i/mg/3/50/526548a343e4b';
+  const EXTENSION = 'jpg';
+  const VARIANTS: PicVariantName[] = ['standard_fantastic', 'standard_xlarge'];
+
+  render(<Image title={TITLE} alt={ALT} path={PATH} extension={EXTENSION} sizing={VARIANTS} />);
+  const img = screen.getByRole('img', { name: /the pic of spiderman/i });
+
+  expect(img.getAttribute('srcset')).toEqual(
+    `${PATH}/standard_xlarge.${EXTENSION} 200w, ${PATH}/standard_fantastic.${EXTENSION} 250w`,
+  );
+});
+
+it('does not render srcset with single size or string', () => {
+  const TITLE = 'Spiderman';
+  const ALT = 'The pic of Spiderman';
+  const PATH = 'http://i.annihil.us/u/prod/marvel/i/mg/3/50/526548a343e4b';
+  const EXTENSION = 'jpg';
+  const VARIANT: PicVariantName = 'standard_xlarge';
+
+  render(<Image title={TITLE} alt={ALT} path={PATH} extension={EXTENSION} sizing={VARIANT} />);
+  const img = screen.getByRole('img', { name: /the pic of spiderman/i });
+
+  expect(img.getAttribute('srcset')).toBeNull();
+});
+
+it('outputs src of the biggest image when multiple sizes are provided', () => {
+  const TITLE = 'Spiderman';
+  const ALT = 'The pic of Spiderman';
+  const PATH = 'http://i.annihil.us/u/prod/marvel/i/mg/3/50/526548a343e4b';
+  const EXTENSION = 'jpg';
+  const VARIANTS: PicVariantName[] = ['standard_xlarge', 'standard_fantastic'];
+
+  render(<Image title={TITLE} alt={ALT} path={PATH} extension={EXTENSION} sizing={VARIANTS} />);
+  const img = screen.getByRole('img', { name: /the pic of spiderman/i });
+
+  expect(img.getAttribute('src')).toEqual(`${PATH}/standard_fantastic.${EXTENSION}`);
+});
+
+it('outputs src of the image when single size is provided', () => {
+  const TITLE = 'Spiderman';
+  const ALT = 'The pic of Spiderman';
+  const PATH = 'http://i.annihil.us/u/prod/marvel/i/mg/3/50/526548a343e4b';
+  const EXTENSION = 'jpg';
+  const VARIANT: PicVariantName = 'standard_xlarge';
+
+  render(<Image title={TITLE} alt={ALT} path={PATH} extension={EXTENSION} sizing={VARIANT} />);
+  const img = screen.getByRole('img', { name: /the pic of spiderman/i });
+
+  expect(img.getAttribute('src')).toEqual(`${PATH}/standard_xlarge.${EXTENSION}`);
+});
+
+it('outputs src of the image when only single size is provided inside a collection', () => {
+  const TITLE = 'Spiderman';
+  const ALT = 'The pic of Spiderman';
+  const PATH = 'http://i.annihil.us/u/prod/marvel/i/mg/3/50/526548a343e4b';
+  const EXTENSION = 'jpg';
+  const VARIANT: PicVariantName[] = ['standard_xlarge'];
+
+  render(<Image title={TITLE} alt={ALT} path={PATH} extension={EXTENSION} sizing={VARIANT} />);
+  const img = screen.getByRole('img', { name: /the pic of spiderman/i });
+
+  expect(img.getAttribute('src')).toEqual(`${PATH}/standard_xlarge.${EXTENSION}`);
+});
+
+it('does not set sizes attribute when sizing is a single size', () => {
+  const TITLE = 'Spiderman';
+  const ALT = 'The pic of Spiderman';
+  const PATH = 'http://i.annihil.us/u/prod/marvel/i/mg/3/50/526548a343e4b';
+  const EXTENSION = 'jpg';
+  const VARIANT: PicVariantName = 'standard_xlarge';
+
+  render(<Image title={TITLE} alt={ALT} path={PATH} extension={EXTENSION} sizing={VARIANT} />);
+  const img = screen.getByRole('img', { name: /the pic of spiderman/i });
+
+  expect(img.getAttribute('sizes')).toBeNull();
+});
+
+it('correctly sets sizes attribute when sizing is an array with 2 unique sizes', () => {
+  const TITLE = 'Spiderman';
+  const ALT = 'The pic of Spiderman';
+  const PATH = 'http://i.annihil.us/u/prod/marvel/i/mg/3/50/526548a343e4b';
+  const EXTENSION = 'jpg';
+  const VARIANTS: PicVariantName[] = ['standard_xlarge', 'standard_fantastic'];
+
+  render(<Image title={TITLE} alt={ALT} path={PATH} extension={EXTENSION} sizing={VARIANTS} />);
+  const img = screen.getByRole('img', { name: /the pic of spiderman/i });
+
+  expect(img.getAttribute('sizes')).toEqual('(max-width: 768px) 200px, 250px');
 });
