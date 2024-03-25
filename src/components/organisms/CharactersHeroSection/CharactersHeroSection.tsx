444f377fe057438bc33056afcaef33e1fdaad39d
ElDav1d <dvivoes@yahoo.es>
2024-03-25 14:37:51 +0100

    FIX vertical spacing

--- src/components/organisms/CharactersHeroSection/CharactersHeroSection.tsx
+++ src/components/organisms/CharactersHeroSection/CharactersHeroSection.tsx
@@ -6,7 +6,7 @@ const CharactersHeroSection = ({ children }: { children: ReactNode }) => (
     className='relative flex flex-col items-center justify-around pt-logoDefaultHeight gap-2 min-h-52 md:min-h-72 mb-4 bg-black bg-hero-image bg-center bg-cover
     before:absolute before:content[""] before:bg-gradient-to-b before:from-trans-0.5-black before:from-50% before:left-0 before:top-0 before:z-[-1]'
   >
-    <Container element='div' className='lg:w-2/3 flex flex-col gap-4'>
+    <Container element='div' className='my-4 lg:w-2/3 flex flex-col gap-4'>
       <div className='text-white text-center'>
         <h2 className='text-2xl md:text-3xl font-semibold text-white text-center uppercase mb-1'>
           Marvel Characters
