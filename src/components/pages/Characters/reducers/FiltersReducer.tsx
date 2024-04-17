5709eed5b8d74c19faff86b046a762506b5d4b6b
ElDav1d <dvivoes@yahoo.es>
2024-04-17 11:58:15 +0200

    local storage WIP 3

--- src/components/pages/Characters/reducers/FiltersReducer.tsx
+++ src/components/pages/Characters/reducers/FiltersReducer.tsx
@@ -6,10 +6,14 @@ export type FiltersAction =
   | { type: 'SET_FILTER'; filter: FilterCriteriaType; isChecked: boolean }
   | { type: 'CLEAR_FILTERS' };
 
-export const initialFiltersState: FiltersState = {
-  withImage: false,
-  withDescription: false,
-};
+const storageItem = localStorage.getItem('charactersContext');
+
+export const initialFiltersState: FiltersState = storageItem
+  ? JSON.parse(storageItem).filters
+  : {
+      withImage: false,
+      withDescription: false,
+    };
 
 const filtersReducer = (state: FiltersState, action: FiltersAction) => {
   switch (action.type) {
